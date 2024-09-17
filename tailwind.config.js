6c7c20ac946d553cdc4da94bce87c2fa4751b524
shivdutt-B <shivdutt059@gmail.com>
2024-09-17 19:11:07 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        header: "var(--theme-color)",
+        'theme-color': 'var(--theme-color)',
+        'bg': 'var(--bg)',
+        'darkTxt': 'var(--dark-txt)',
+        'lightTxt': 'var(--light-text)'
+      },
+      fontSize: {
+        'primary': '100px',
+        'maxHeading': 'var(--max-heading)',
+        'midHeading': 'var(--mid-heading)',
+        'minHeading': 'var(--min-heading)'
+      },
+      fontWeight: {
+        'minWeight': 'var(--min-weight)',
+        'midWeight': 'var(--mid-weight)',
+        'maxWeight': 'var(max-weight)'
+      },
+      fontFamily: {
+        'globalFontFam': 'var(--global-font-family)'
+      }
+    },
+  },
+  plugins: [],
+}
+
