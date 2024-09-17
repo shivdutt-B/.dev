5323f96042ecf7c2a5e1cfbc151b42ff74f30d13
shivdutt-B <shivdutt059@gmail.com>
2024-09-17 21:56:56 +0530

    intro section complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,17 +4,18 @@ function Navbar() {
   return (
     <div>
       <nav class="flex justify-between items-center p-5 px-[3vw] shadow-[0px_4px_7px_1px_rgba(0,_0,_0,_0.1)]">
-        <div class="font-[var(--max-weight)] text-[25px] text-[var(--dark-txt)] font-[var(--max-heading)] fluid-txt-nav-head cursor-pointer flex items-center">
+        <div class="font-[var(--max-weight)] text-[25px] text-[var(--dark-txt)] 
+        text-[clamp(25px,2vw,48px)] cursor-pointer flex items-center">
           <span className="relative top-1 right-[-5px]">
             <svg width="27px" height="auto" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.032"></g><g id="SVGRepo_iconCarrier"><path fill="#8144e5" d="M8 3a5 5 0 100 10A5 5 0 008 3z"></path></g></svg>
           </span>
           dev
         </div>
         <div class="flex items-center space-x-8">
-          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[var(--primary-font-size)] fluid-txt-nav-opt">Home</a>
-          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[var(--primary-font-size)] fluid-txt-nav-opt">About</a>
-          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[var(--primary-font-size)] fluid-txt-nav-opt">Projects</a>
-          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[var(--primary-font-size)] fluid-txt-nav-opt">Contact</a>
+          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[clamp(16px,1.2vw,20px)]">Home</a>
+          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[clamp(16px,1.2vw,20px)]">About</a>
+          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[clamp(16px,1.2vw,20px)]">Projects</a>
+          <a href="#" class="text-[var(--light-txt)] font-[var(--mid-weight)] text-[clamp(16px,1.2vw,20px)]">Contact</a>
         </div>
       </nav>
     </div>
