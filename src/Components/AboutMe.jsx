1f8ed0d960912c7be5b6cdfc80f625a206ccee71
shivdutt-B <shivdutt059@gmail.com>
2024-09-21 01:29:42 +0530

    about me responsiveness completed

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import Me from "../assets/color_stripe_crop.jpg"
+import React from 'react';
+import Me from "../assets/color_stripe_crop.jpg";
 
 function Intro() {
     return (
-        <div className="flex justify-center my-40" id="about">
-            <div class="grid grid-cols-2 max-w-[1000px] w-[100%]">
-                <div class="">
-                    <div class="">
-                        <img class="rounded-md" src={Me}></img>
-                    </div>
+        <div className="flex justify-center my-40 px-5" id="about">
+            {/* Flexbox container with responsive layout */}
+            <div className="flex flex-col-reverse md:flex-row max-w-[1000px] w-full gap-8">
+                
+                {/* Left Side (Image Section) */}
+                <div className="flex justify-center md:justify-start">
+                    <img 
+                        className="rounded-md w-full h-auto md:w-[500px] object-cover" 
+                        src={Me} 
+                        alt="Shivdutt Bhadakwad" 
+                    />
                 </div>
-                <div class="flex flex-col justify-center">
-                    <h1 class="text-[clamp(30px,10vw,40px)] font-[var(--max-weight)] text-[var(--dark-txt)] mb-4">
-                        About me
+                
+                {/* Right Side (Text Section) */}
+                <div className="flex flex-col justify-center">
+                    <h1 className="text-[clamp(30px,8vw,40px)] font-[var(--max-weight)] text-[var(--dark-txt)] mb-4">
+                        About Me
                     </h1>
-                    <p class="text-[var(--light-txt)] mb-8 text-[clamp(16px,1.2vw,20px)]">
+                    <p className="text-[var(--light-txt)] mb-8 text-[clamp(16px,1.5vw,20px)] leading-relaxed">
                         Hi, I'm Shivdutt Bhadakwad, a Fullstack Developer with a strong passion for Machine Learning.
-                        <br></br>
-                        <br></br>
+                        <br /><br />
                         Currently, I'm in my second year of studying Computer Engineering in Pune, India, where I'm actively building my skills and knowledge in both web development and cutting-edge technologies like AI.
                     </p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Intro
+export default Intro;
