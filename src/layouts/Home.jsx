9b932a77e713f55ddaab9a9fc5ff20582d4e83fd
shivdutt-B <shivdutt059@gmail.com>
2025-08-25 02:32:43 +0530

    casing issue in vercel

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import Intro from '../components/Intro'
-import Skills from 'src/components/Skills'
-import Projects from '../Components/Projects'
-import { DSAStats } from '@/components/DSA'
+import Intro from '../components/Intro'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import { DSAStats } from '../components/DSA'
 import {AppNavbar} from '../components/Navbar'
 import Contact from '../components/Contact'
 
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div className='min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6'>
       <AppNavbar />
-      {/* <Intro /> */}
+      <Intro />
       <Skills />
       <Projects />
       <DSAStats />
